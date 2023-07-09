97fe495f0f6fa477a87967f95092a4c4c25a9bef
Hugo Bouderlique <bouderliqueh@gmail.com>
2023-07-10 00:15:26 +1000

    update readme and format code a little better

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 # BLOG_BOB
 
 ## What is blog_bob
-`blob_bob` is a microservice I developed to support my continuously deployed blog. It monitors a folder for new or updated markdown files, and converts them to HTML blog articles in sub millisecond time (that's not actually true yet). It also updates a supporting PostgreSQL database which catalogues all the articles and helps the main backend server render the blog homepage.
+`blob_bob` is a microservice I developed to support my continuously deployed blog. It monitors a folder for new or updated markdown files, and converts them to HTML blog articles in sub millisecond time. It also updates a supporting PostgreSQL database which catalogues all the articles and helps the main backend server render the blog homepage.
 
 ## Why the name "blog_bob"?
 I am a huge fan of Bob the Builder, and attribute my "can-do" attitude and love for tinkering and building things to him. Since `blog_bob` helps me **build** my blog, I wanted to dedicate this project to Bob the Builder as a thank you for the positive impact he has had on my life.
--- src/bob.rs
+++ src/bob.rs
@@ -6,22 +6,23 @@ use configparser::ini::Ini;
 use inotify::{Event, EventMask, Events, Inotify, WatchMask};
 use postgres::{Client, Config, NoTls};
 use pulldown_cmark::html::push_html;
-use pulldown_cmark::Options as MarkdownOptions;
-use pulldown_cmark::Parser;
+use pulldown_cmark::{Options as MarkdownOptions, Parser};
 use serde::Deserialize;
 use time::Date;
 
 pub struct Bob {
-    inotify: Inotify,
+    inotify:     Inotify,
     monitor_dir: PathBuf,
-    output_dir: PathBuf,
-    db_client: Client,
-    md_options: MarkdownOptions,
+    output_dir:  PathBuf,
+    db_client:   Client,
+    md_options:  MarkdownOptions,
 }
 
 impl Bob {
     pub fn new(config_path: &str) -> Result<Bob, Box<dyn Error>> {
+
         let mut config_parser = Ini::new();
+
         config_parser.load(config_path)?;
 
         let monitor_dir = config_parser
@@ -34,26 +35,14 @@ impl Bob {
 
         let inotify = Inotify::init()?;
 
-        inotify
-            .watches()
-            .add(&monitor_dir, WatchMask::ALL_EVENTS | WatchMask::ONLYDIR)?;
+        inotify.watches().add(&monitor_dir, WatchMask::ALL_EVENTS | WatchMask::ONLYDIR)?;
 
         let mut db_config = Config::new();
 
         db_config
-            .host(
-                config_parser
-                    .get("database", "host")
-                    .unwrap_or(String::from("localhost"))
-                    .as_str(),
-            )
+            .host(config_parser.get("database", "host").unwrap_or(String::from("localhost")).as_str())
             .port(config_parser.getuint("database", "port")?.unwrap_or(5432) as u16)
-            .user(
-                config_parser
-                    .get("database", "user")
-                    .ok_or("Missing \"user\" parameter in \"database\" section")?
-                    .as_str(),
-            )
+            .user(config_parser.get("database", "user").ok_or("Missing \"user\" parameter in \"database\" section")?.as_str())
             .password(
                 config_parser
                     .get("database", "password")
@@ -70,6 +59,7 @@ impl Bob {
         let mut md_options = MarkdownOptions::empty();
 
         md_options.insert(MarkdownOptions::ENABLE_STRIKETHROUGH);
+
         md_options.insert(MarkdownOptions::ENABLE_TABLES);
 
         Ok(Bob {
@@ -77,23 +67,22 @@ impl Bob {
             monitor_dir: PathBuf::from(monitor_dir),
             output_dir: PathBuf::from(output_dir),
             db_client: db_config.connect(NoTls)?,
-            md_options: md_options,
+            md_options,
         })
     }
 
     pub fn run(&mut self) {
+
         let mut buffer = [0; 1024];
 
-        println!(
-            "Bob monitoring {} and writing to {}...",
-            self.monitor_dir.display(),
-            self.output_dir.display()
-        );
+        println!("Bob monitoring {} and writing to {}...", self.monitor_dir.display(), self.output_dir.display());
 
         loop {
+
             match self.inotify.read_events_blocking(&mut buffer) {
                 Ok(events) => {
                     if let Err(error) = self.process_events(events) {
+
                         eprintln!("{}", error);
                     }
                 }
@@ -103,30 +92,38 @@ impl Bob {
     }
 
     fn process_events(&mut self, events: Events<'_>) -> Result<(), Box<dyn Error>> {
+
         for event in events {
-            match event.mask {
-                EventMask::MODIFY => self.update_article(event)?,
-                EventMask::DELETE => self.delete_article(event)?,
+
+            let start = std::time::Instant::now();
+
+            match &event.mask {
+                &EventMask::MODIFY => self.update_article(&event)?,
+                &EventMask::DELETE => self.delete_article(&event)?,
                 _ => {}
             }
+
+            let elapsed = start.elapsed();
+
+            println!("event {:?} in {}", event.mask, elapsed.as_micros());
         }
 
         Ok(())
     }
 
-    fn update_article(&mut self, event: Event<&OsStr>) -> Result<(), Box<dyn Error>> {
+    fn update_article(&mut self, event: &Event<&OsStr>) -> Result<(), Box<dyn Error>> {
+
         let mut input_file_name = PathBuf::from(&self.monitor_dir);
 
         input_file_name.push(event.name.unwrap());
 
         let file_content = std::fs::read_to_string(&input_file_name).unwrap();
 
+        let start = std::time::Instant::now();
+
         let mut parts = file_content.splitn(3, "---");
 
-        let frontmatter_content = parts.nth(1).ok_or(format!(
-            "Missing frontmatter in {}",
-            input_file_name.display()
-        ))?;
+        let frontmatter_content = parts.nth(1).ok_or(format!("Missing frontmatter in {}", input_file_name.display()))?;
 
         let md_content = parts
             .nth(0) // Calling nth consumes all items up to and including the one returned. This
@@ -143,19 +140,19 @@ impl Bob {
 
         push_html(&mut html_content, md_parser);
 
+        println!("$$$$$$ {}", start.elapsed().as_micros());
+
         let mut output_file_name = PathBuf::from(&self.output_dir);
+
         output_file_name.push(event.name.unwrap());
+
         output_file_name.set_extension("html");
+
         std::fs::write(&output_file_name, html_content).unwrap();
 
         self.db_client.execute(
             "INSERT INTO blog_articles VALUES(DEFAULT, $1, $2, $3 , CURRENT_DATE, $4);",
-            &[
-                &article_metadata.title,
-                &output_file_name.to_str(),
-                &article_metadata.pub_date,
-                &article_metadata.tags,
-            ],
+            &[&article_metadata.title, &output_file_name.to_str(), &article_metadata.pub_date, &article_metadata.tags],
         )?;
 
         println!("Updated article \"{}\"", article_metadata.title);
@@ -163,19 +160,15 @@ impl Bob {
         Ok(())
     }
 
-    fn delete_article(&mut self, event: Event<&OsStr>) -> Result<(), Box<dyn Error>> {
+    fn delete_article(&mut self, event: &Event<&OsStr>) -> Result<(), Box<dyn Error>> {
+
         let mut file_name = PathBuf::from(event.name.unwrap());
+
         file_name.set_extension("");
 
-        self.db_client.execute(
-            "DELETE FROM blog_articles WHERE path ~ 'shit';",
-            &[/* &file_name.to_str() */],
-        )?;
+        self.db_client.execute("DELETE FROM blog_articles WHERE path ~ 'shit';", &[/* &file_name.to_str() */])?;
 
-        println!(
-            "Deleted article \"{}\"",
-            event.name.unwrap().to_string_lossy()
-        );
+        println!("Deleted article \"{}\"", event.name.unwrap().to_string_lossy());
 
         Ok(())
     }
@@ -183,11 +176,12 @@ impl Bob {
 
 #[allow(dead_code)]
 #[derive(Debug, Deserialize)]
+
 struct ArticleMetadata {
-    title: String,
+    title:    String,
     #[serde(with = "date_format")]
     pub_date: Date,
-    tags: Vec<String>,
+    tags:     Vec<String>,
 }
 
 time::serde::format_description!(date_format, Date, "[year]-[month]-[day]");
