481cb219a80d397fec034a2e34afa4a73df02d78
Hugo Bouderlique <bouderliqueh@gmail.com>
2023-07-24 20:59:11 +1000

    code formatting

--- src/bob.rs
+++ src/bob.rs
@@ -11,16 +11,15 @@ use serde::Deserialize;
 use time::Date;
 
 pub struct Bob {
-    inotify:     Inotify,
+    inotify: Inotify,
     monitor_dir: PathBuf,
-    output_dir:  PathBuf,
-    db_client:   Client,
-    md_options:  MarkdownOptions,
+    output_dir: PathBuf,
+    db_client: Client,
+    md_options: MarkdownOptions,
 }
 
 impl Bob {
     pub fn new(config_path: &str) -> Result<Bob, Box<dyn Error>> {
-
         let mut config_parser = Ini::new();
 
         config_parser.load(config_path)?;
@@ -35,14 +34,26 @@ impl Bob {
 
         let inotify = Inotify::init()?;
 
-        inotify.watches().add(&monitor_dir, WatchMask::ALL_EVENTS | WatchMask::ONLYDIR)?;
+        inotify
+            .watches()
+            .add(&monitor_dir, WatchMask::ALL_EVENTS | WatchMask::ONLYDIR)?;
 
         let mut db_config = Config::new();
 
         db_config
-            .host(config_parser.get("database", "host").unwrap_or(String::from("localhost")).as_str())
+            .host(
+                config_parser
+                    .get("database", "host")
+                    .unwrap_or(String::from("localhost"))
+                    .as_str(),
+            )
             .port(config_parser.getuint("database", "port")?.unwrap_or(5432) as u16)
-            .user(config_parser.get("database", "user").ok_or("Missing \"user\" parameter in \"database\" section")?.as_str())
+            .user(
+                config_parser
+                    .get("database", "user")
+                    .ok_or("Missing \"user\" parameter in \"database\" section")?
+                    .as_str(),
+            )
             .password(
                 config_parser
                     .get("database", "password")
@@ -72,17 +83,18 @@ impl Bob {
     }
 
     pub fn run(&mut self) {
-
         let mut buffer = [0; 1024];
 
-        println!("Bob monitoring {} and writing to {}...", self.monitor_dir.display(), self.output_dir.display());
+        println!(
+            "Bob monitoring {} and writing to {}...",
+            self.monitor_dir.display(),
+            self.output_dir.display()
+        );
 
         loop {
-
             match self.inotify.read_events_blocking(&mut buffer) {
                 Ok(events) => {
                     if let Err(error) = self.process_events(events) {
-
                         eprintln!("{}", error);
                     }
                 }
@@ -92,38 +104,30 @@ impl Bob {
     }
 
     fn process_events(&mut self, events: Events<'_>) -> Result<(), Box<dyn Error>> {
-
         for event in events {
-
-            let start = std::time::Instant::now();
-
             match &event.mask {
                 &EventMask::MODIFY => self.update_article(&event)?,
                 &EventMask::DELETE => self.delete_article(&event)?,
                 _ => {}
             }
-
-            let elapsed = start.elapsed();
-
-            println!("event {:?} in {}", event.mask, elapsed.as_micros());
         }
 
         Ok(())
     }
 
     fn update_article(&mut self, event: &Event<&OsStr>) -> Result<(), Box<dyn Error>> {
-
         let mut input_file_name = PathBuf::from(&self.monitor_dir);
 
         input_file_name.push(event.name.unwrap());
 
         let file_content = std::fs::read_to_string(&input_file_name).unwrap();
 
-        let start = std::time::Instant::now();
-
         let mut parts = file_content.splitn(3, "---");
 
-        let frontmatter_content = parts.nth(1).ok_or(format!("Missing frontmatter in {}", input_file_name.display()))?;
+        let frontmatter_content = parts.nth(1).ok_or(format!(
+            "Missing frontmatter in {}",
+            input_file_name.display()
+        ))?;
 
         let md_content = parts
             .nth(0) // Calling nth consumes all items up to and including the one returned. This
@@ -140,8 +144,6 @@ impl Bob {
 
         push_html(&mut html_content, md_parser);
 
-        println!("$$$$$$ {}", start.elapsed().as_micros());
-
         let mut output_file_name = PathBuf::from(&self.output_dir);
 
         output_file_name.push(event.name.unwrap());
@@ -152,7 +154,12 @@ impl Bob {
 
         self.db_client.execute(
             "INSERT INTO blog_articles VALUES(DEFAULT, $1, $2, $3 , CURRENT_DATE, $4);",
-            &[&article_metadata.title, &output_file_name.to_str(), &article_metadata.pub_date, &article_metadata.tags],
+            &[
+                &article_metadata.title,
+                &output_file_name.to_str(),
+                &article_metadata.pub_date,
+                &article_metadata.tags,
+            ],
         )?;
 
         println!("Updated article \"{}\"", article_metadata.title);
@@ -161,14 +168,19 @@ impl Bob {
     }
 
     fn delete_article(&mut self, event: &Event<&OsStr>) -> Result<(), Box<dyn Error>> {
-
         let mut file_name = PathBuf::from(event.name.unwrap());
 
         file_name.set_extension("");
 
-        self.db_client.execute("DELETE FROM blog_articles WHERE path ~ 'shit';", &[/* &file_name.to_str() */])?;
+        self.db_client.execute(
+            "DELETE FROM blog_articles WHERE path ~ 'shit';",
+            &[/* &file_name.to_str() */],
+        )?;
 
-        println!("Deleted article \"{}\"", event.name.unwrap().to_string_lossy());
+        println!(
+            "Deleted article \"{}\"",
+            event.name.unwrap().to_string_lossy()
+        );
 
         Ok(())
     }
@@ -178,10 +190,10 @@ impl Bob {
 #[derive(Debug, Deserialize)]
 
 struct ArticleMetadata {
-    title:    String,
+    title: String,
     #[serde(with = "date_format")]
     pub_date: Date,
-    tags:     Vec<String>,
+    tags: Vec<String>,
 }
 
 time::serde::format_description!(date_format, Date, "[year]-[month]-[day]");
